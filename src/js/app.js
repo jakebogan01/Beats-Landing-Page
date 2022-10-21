1c727926bfd004ec8ad68528a9520636b113cd2d
Jake Bogan <jakebogan01@gmail.com>
2022-10-21 14:38:39 -0400

    adds mouse light to body

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,20 @@
 AOS.init();
 
+const handleOnMouseMove = e => {
+    const { currentTarget: target } = e;
+
+    const rect = target.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+    target.style.setProperty("--mouse-x", `${x}px`);
+    target.style.setProperty("--mouse-y", `${y}px`);
+}
+
+for(const light of document.querySelectorAll(".light")) {
+    light.onmousemove = e => handleOnMouseMove(e);
+}
+
 let image = document.getElementsByClassName('parallaximg1');
 new simpleParallax(image, {
     scale: 1.2,
